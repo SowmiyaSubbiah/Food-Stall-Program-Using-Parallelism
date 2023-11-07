85e6e1033e21dc17c8c737967ca74c95f00a1286
Sowmiya <sowmiyasubbiah8@gmail.com>
2023-11-08 04:43:14 +0530

    Commit 1

--- FoodStall.java
+++ FoodStall.java
@@ -3,7 +3,7 @@ import java.util.concurrent.Executors;
 
 class FoodStall
 {
-    public void takeOrder(String custName, String foodItem)
+    public void takeOrder(String custName, String foodItem) // Method Defined
     {
         System.out.println(custName + " is ordering " + foodItem);
         System.out.println();
@@ -16,15 +16,15 @@ class FoodStall
             e.printStackTrace();
         }
         System.out.println(foodItem + " is ready for "+custName);
-        System.out.println();
+        System.out.println("");
     }
 }
 class custOrder implements Runnable
 {
     private String custName, foodItem;
-    private FoodStall foodStall;
+    private FoodStall foodStall; // Reference Variable for the Class FoodStall
 
-public custOrder(String custName, String foodItem, FoodStall foodStall)
+public custOrder(String custName, String foodItem, FoodStall foodStall) // Constructor
 {
     this.custName = custName;
     this.foodItem = foodItem;
@@ -38,14 +38,14 @@ foodStall.takeOrder(custName, foodItem);
 
 public static void main(String[] args)
 {
-    FoodStall fs = new FoodStall();
-    ExecutorService executor = Executors.newFixedThreadPool(5);
-    String[] customers = {"customer 1", "customer 2", "customer 3", "customer 4", "customer 5"};
+    FoodStall fs = new FoodStall(); // Creating Object for the Class FoodStall
+    ExecutorService executor = Executors.newFixedThreadPool(5); // Creating fixed Threads with Executor Framework
+    String[] customers = {"customer 1", "customer 2", "customer 3", "customer 4", "customer 5"}; // Creating Array of Strings
     String[] orders = {"Burger", "Pasta", "Noodles", "Briyani", "Fried Rice"};
 
     for(int i=0; i<5; i++)
     {
-        Runnable order = new custOrder(customers[i], orders[i], fs);
+        Runnable order = new custOrder(customers[i], orders[i], fs); // Creating Object for the Runnable Interface
         executor.execute(order);
     }
     executor.shutdown();
